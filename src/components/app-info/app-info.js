69c848b78c7f287407b697bb811b59cd2241ea2d
artemplohenko@gmail.com <artemplohenko@gmail.com>
2023-10-18 19:00:54 +0300

    created components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Accounting of company employees</h1>
+      <h2>Total number of employees: </h2>
+      <h2>Receive a premium: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
