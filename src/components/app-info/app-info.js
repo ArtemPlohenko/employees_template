ad174ac3c9bde0fa09a5434bfd723731bf616e82
artemplohenko@gmail.com <artemplohenko@gmail.com>
2023-10-23 17:38:09 +0300

    Rise of state 2

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({ increased, employees }) => {
   return (
     <div className="app-info">
       <h1>Accounting of company employees</h1>
-      <h2>Total number of employees: </h2>
-      <h2>Receive a premium: </h2>
+      <h2>Total number of employees: {employees}</h2>
+      <h2>Receive a premium: {increased}</h2>
     </div>
   );
 };
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,7 +3,8 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
 // This is the EmployeesList component that receives data as a prop.
-const EmployeesList = ({ data, onDelete, onToggleIncrease, onToggleRise }) => {
+// const EmployeesList = ({ data, onDelete, onToggleIncrease, onToggleRise }) => {
+const EmployeesList = ({ data, onDelete, onToggleProp }) => {
   // Map through the data array to create a list of EmployeeListItem components.
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
@@ -13,8 +14,9 @@ const EmployeesList = ({ data, onDelete, onToggleIncrease, onToggleRise }) => {
         key={id}
         {...itemProps}
         onDelete={() => onDelete(id)}
-        onToggleIncrease={() => onToggleIncrease(id)}
-        onToggleRise={() => onToggleRise(id)}
+        // onToggleIncrease={() => onToggleIncrease(id)}
+        // onToggleRise={() => onToggleRise(id)}
+        onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute("data-toggle"))}
       />
     );
   });
