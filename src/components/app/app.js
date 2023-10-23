f0430d9b2934ef93b5edca7d028a8776c26349a0
artemplohenko@gmail.com <artemplohenko@gmail.com>
2023-10-23 17:00:26 +0300

    added change state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -47,18 +47,24 @@ class App extends Component {
 
   onToggleIncrease = (id) => {
     // console.log(`Increase this ${id}`);
-    this.setState(({ data }) => {
-      const index = data.findIndex((elem) => elem.id === id);
+    // this.setState(({ data }) => {
+    //   const index = data.findIndex((elem) => elem.id === id);
+    //   const old = data[index];
+    //   const newItem = { ...old, increase: !old.increase };
+    //   const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+    //   return {
+    //     data: newArr,
+    //   };
+    // });
 
-      const old = data[index];
-      const newItem = { ...old, increase: !old.increase };
-
-      const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-
-      return {
-        data: newArr,
-      };
-    });
+    this.setState(({ data }) => ({
+      data: data.map((item) => {
+        if (item.id === id) {
+          return { ...item, increase: !item.increase };
+        }
+        return item;
+      }),
+    }));
   };
 
   onToggleRise = (id) => {
